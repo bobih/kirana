c12d198b6b7628516c54108edcc96b21b4cd671d
bobih <bobby.khrisna@gmail.com>
2026-04-09 13:31:24 +0700

    update Images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,10 @@
 <body class="bg-gray-900">
     <!-- Header -->
     <header class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-        <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        <img src="{{ asset('images/background.avif') }}"
             alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center" />
-        <div aria-hidden="true"
+        
+            <div aria-hidden="true"
             class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
             <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
                 class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"></div>
@@ -101,9 +102,11 @@
                         <div class="flex items-center justify-between">
                             <a href="#" class="-m-1.5 p-1.5">
                                 <span class="sr-only">Your Company</span>
-                                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                <img src="{{ asset('images/mark.svg') }}"
                                     alt="" class="h-8 w-auto" />
-                            </a>
+                                
+
+                                </a>
                             <button type="button" command="close" commandfor="mobile-menu"
                                 class="-m-2.5 rounded-md p-2.5 text-gray-200">
                                 <span class="sr-only">Close menu</span>
@@ -193,7 +196,7 @@
                         <div class="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
                             <div
                                 class="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 outline outline-white/20">
-                                <img src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                                <img src="{{asset('images/bento-03-mobile-friendly.png')}}"
                                     alt="" class="size-full object-cover object-top" />
                             </div>
                         </div>
@@ -214,7 +217,7 @@
                         </div>
                         <div
                             class="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-                            <img src="https://tailwindcss.com/plus-assets/img/component-images/dark-bento-03-performance.png"
+                            <img src="{{asset('images/dark-bento-03-performance.png')}}"
                                 alt="" class="w-full max-lg:max-w-xs" />
                         </div>
                     </div>
@@ -232,7 +235,7 @@
                                 arcu sed. Tellus semper adipiscing suspendisse semper morbi.</p>
                         </div>
                         <div class="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                            <img src="https://tailwindcss.com/plus-assets/img/component-images/dark-bento-03-security.png"
+                            <img src="{{asset('images/dark-bento-03-security.png')}}"
                                 alt="" class="h-[min(152px,40cqw)] object-cover" />
                         </div>
                     </div>
